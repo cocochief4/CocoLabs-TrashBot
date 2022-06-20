e1b6e12be997d5632db6fae228b4d590114750f6
Cocochief <cocochief4@gmail.com>
2022-06-19 18:33:13 -0700

    Arduino Slave now sends PWM from RC

--- ArduinoUno/Arduino-Slave/Arduino-Slave/Arduino-Slave.ino
+++ ArduinoUno/Arduino-Slave/Arduino-Slave/Arduino-Slave.ino
@@ -8,25 +8,58 @@
 // Created 29 March 2006
 
 // This example code is in the public domain.
+int Ch1Pin = 5;
+int Ch2Pin = 6;
+
+int Ch1Val;
+int Ch2Val;
+
+String finalSend;
 
 int requestCount = 0;
 #include <Wire.h>
 
 void setup() {
-  Wire.begin(8);                // join i2c bus with address #8
+  Wire.begin(4);                // join i2c bus with address #8
   Wire.onRequest(requestEvent); // register event
   Serial.begin(9600);
+
+  pinMode(Ch1Pin, INPUT);
+  pinMode(Ch2Pin, INPUT);
 }
 
 void loop() {
-  delay(100);
-  Serial.println(requestCount);
+  delay(10);
+  //Serial.println(requestCount);
+  Ch1Val = pulseIn(Ch1Pin, HIGH);
+  Ch2Val = pulseIn(Ch2Pin, HIGH);
+
+  if (Ch1Val < 1000) {
+    Ch1Val = 1000;
+  } else if (Ch1Val > 2000) {
+    Ch1Val = 2000;
+  } else if (Ch1Val > 1460 && Ch1Val < 1540) {
+    Ch1Val = 1500;
+  }
+  
+  if (Ch2Val < 1000) {
+    Ch2Val = 1000;
+  } else if (Ch2Val > 2000) {
+    Ch2Val = 2000;
+  } else if (Ch2Val > 1460 && Ch2Val < 1540) {
+    Ch2Val = 1500;
+  }
+
+  finalSend = String(Ch1Val) + String(Ch2Val);
+  Serial.println(finalSend);
 }
 
 // function that executes whenever data is requested by master
 // this function is registered as an event, see setup()
 void requestEvent() {
-  Wire.write("hello "); // respond with message of 6 bytes
-  // as expected by master
-  requestCount++;
+  char buffer[32];
+  finalSend.toCharArray(buffer, 32);
+  Wire.write(buffer); // 
+  //
+  //requestCount++;
 }
