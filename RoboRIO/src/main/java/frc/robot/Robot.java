fae641c1f382583f0278f9a300a038fadeff325e
cqtqlyst <aayush.gaywala@gmail.com>
2022-06-19 17:09:51 -0700

    move over code

--- RoboRIO/src/main/java/frc/robot/Robot.java
+++ RoboRIO/src/main/java/frc/robot/Robot.java
@@ -7,55 +7,124 @@
 
 package frc.robot;
 
+import java.util.*;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+
+import java.nio.ByteBuffer;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C.*;
+
 public class Robot extends TimedRobot {
   private DifferentialDrive m_myRobot;
+
   private Joystick m_leftStick;
   private Joystick m_rightStick;
-  private static final int leftDeviceID = 1; 
-  private static final int rightDeviceID = 2;
-  private CANSparkMax m_leftMotor;
-  private CANSparkMax m_rightMotor;
+
+  private static final int leftUpDeviceID = 1; 
+  private static final int leftDownDeviceID = 2;
+  private CANSparkMax leftUpMotor;
+  private CANSparkMax leftDownMotor;
+  private MotorControllerGroup leftGroup;
+
+  private static final int rightUpDeviceID = 3;
+  private static final int rightDownDeviceID = 4;
+  private CANSparkMax rightUpMotor;
+  private CANSparkMax rightDownMotor;
+  private MotorControllerGroup rightGroup;
+
+  private final I2C arduino = new I2C(Port.kOnboard, 4);
+
+
+  private DigitalInput testInput = new DigitalInput(0);
+
   @Override
   public void robotInit() {
-  /**
-   * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
-   * 
-   * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
-   * first parameter
-   * 
-   * The motor type is passed as the second parameter. Motor type can either be:
-   *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless
-   *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushed
-   * 
-   * The example below initializes four brushless motors with CAN IDs 1 and 2. Change
-   * these parameters to match your setup
-   */
-    m_leftMotor = new CANSparkMax(leftDeviceID, MotorType.kBrushless);
-    m_rightMotor = new CANSparkMax(rightDeviceID, MotorType.kBrushless);
-
     /**
-     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
-     * in the SPARK MAX to their factory default state. If no argument is passed, these
-     * parameters will not persist between power cycles
+     * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
+     * 
+     * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
+     * first parameter
+     * 
+     * The motor type is passed as the second parameter. Motor type can either be:
+     *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless
+     *  com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushed
+     * 
+     * The example below initializes four brushless motors with CAN IDs 1 and 2. Change
+     * these parameters to match your setup
      */
-    m_leftMotor.restoreFactoryDefaults();
-    m_rightMotor.restoreFactoryDefaults();
+      leftUpMotor = new CANSparkMax(leftUpDeviceID, MotorType.kBrushless);
+      leftDownMotor = new CANSparkMax(leftDownDeviceID, MotorType.kBrushless);
+      
+      rightUpMotor = new CANSparkMax(rightUpDeviceID, MotorType.kBrushless);
+      rightDownMotor = new CANSparkMax(rightDownDeviceID, MotorType.kBrushless);
 
-    m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
+      rightGroup = new MotorControllerGroup(rightUpMotor, rightDownMotor);
+      leftGroup = new MotorControllerGroup(leftUpMotor, leftDownMotor);
 
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
+    
+      
+      /*
+       * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+       * in the SPARK MAX to their factory default state. If no argument is passed, these
+       * parameters will not persist between power cycles
+       */
+      leftUpMotor.restoreFactoryDefaults();
+      leftDownMotor.restoreFactoryDefaults();
+      rightUpMotor.restoreFactoryDefaults();
+      rightDownMotor.restoreFactoryDefaults();
+
+      m_myRobot = new DifferentialDrive(leftGroup, rightGroup);
+
+      m_leftStick = new Joystick(1);
+      m_rightStick = new Joystick(5);
+  }
+  
+  public void teleopInit() {
+    
   }
 
-  @Override
   public void teleopPeriodic() {
-    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+    double throttle = 0;
+    double steering = 0;
+    //Converting from the original values Arduino sends over to -1 to 1 scale
+    if (throttle <= 1460 || throttle >= 1540) {
+      throttle /= 500;
+      throttle -= 3;
+    }
+    else {
+      throttle = 0;
+    }
+    if (steering <= 1460 || throttle >= 1540) {
+      steering /= 500;
+      steering -= 3;
+    }
+    else {
+      steering = 0;
+    }
+    //Converting to polar using (r, theta) | theta is in radians
+    double hypoteneuse = Math.sqrt((throttle*throttle) + (steering*steering));
+    double theta = Math.atan(steering/throttle);
+    //Finding the max length r on the unit circle for a specific angle
+    double rMax;
+    if (theta <= 45) {
+      rMax = (1 / Math.cos(theta));
+    }
+    else if (theta <= 90 && theta > 45) {
+      rMax = (1 / Math.sin(theta));
+    }
+
+    // m_myRobot.tankDrive(.5, .5);
+    // System.out.println(testInput.get());
+    // System.out.println(leftUpMotor.get());
   }
 }
