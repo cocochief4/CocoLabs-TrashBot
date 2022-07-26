674aec7427b45ff6672ebed8d9df92eefbb63353
cqtqlyst <aayush.gaywala@gmail.com>
2022-07-25 17:10:28 -0700

    Create SpanningTreeCoverageAlgorithm.java

---
+++ RoboRIO/src/main/java/frc/robot/SpanningTreeCoverageAlgorithm.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.util.*;
+
+/** Add your docs here. */
+public class SpanningTreeCoverageAlgorithm {
+
+    static class node {
+        //each node is defined as the bottom right corner of the square
+        //we first must generate each node by checking each square and 
+    }
+
+    public static void Main(String[] args) {
+        
+    }
+
+}
