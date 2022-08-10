77a06d1f342e7f4f55fa223d2ecfe4deca334932
Cocochief <cocochief4@gmail.com>
2022-08-10 15:14:45 -0700

    fix it

--- RoboRIO/src/main/java/frc/robot/GPSManager.java
+++ RoboRIO/src/main/java/frc/robot/GPSManager.java
@@ -22,16 +22,83 @@ public class GPSManager {
         return arduinoReceive;
     }
 
-    // protected static LatLongFixStruct ParseGPSData {
-    //     String data = GetDataFromGPS();
-    //     if (data.length() == 29) {
-    //         data = data.substring(0, 29);
-    //         int numOfCommas;
-    //         boolean commas = true;
-    //         while (commas == true) {
-                
-    //         }
-    //     }
-    // }
+    private static long ConvertToLong(String longString) {
+        try {
+            return Long.parseLong(longString);
+
+        } catch (NumberFormatException e) {
+            return 9223372036854775807L;
+        }
+    }
+
+    private static short ConvertToShort(String shortString) {
+        try {
+            return Short.parseShort(shortString);
+
+        } catch (NumberFormatException e) {
+            return (short) 32767;
+        }
+    }
+
+    protected static LatLongFixStruct ParseGPSData(byte callOrigin) {
+        LatLongFixStruct latLongFixStruct = new LatLongFixStruct(0L, 0L, (short)0, (short)0);
+
+        String data = GetDataFromGPS();
+        if (data.length() == sendSize) {
+            data = data.substring(0, sendSize);
+
+            String[] strArr = new String[4];
+            int numOfValues = 0;
+
+            strArr = data.split(",");
+
+            numOfValues = (short) strArr.length;
+
+            System.out.println("size check");
+            System.out.println(numOfValues);
+            System.out.println(GPSManager.GetDataFromGPS().toString());
+
+            if (numOfValues == 4) {
+                long long1 = ConvertToLong(strArr[0]);
+                long long2 = ConvertToLong(strArr[1]);
+                short short1 = ConvertToShort(strArr[2]);
+                short short2 = ConvertToShort(strArr[3]);
+                System.out.println("commas check");
+                System.out.println("Long: " + long1 + ", " + long2);
+
+                if (long1 != 9223372036854775807L && long2 != 9223372036854775807L
+                    && short1 != 32767 && short2 != 32767) {
+                    System.out.println("no corruption");
+                    if (callOrigin == 0) {  // If called from setup
+                        latLongFixStruct.latitude = Long.parseLong(strArr[0]);
+                        latLongFixStruct.longitude = Long.parseLong(strArr[1]);
+                        latLongFixStruct.fix = Short.parseShort(strArr[2]);
+                        latLongFixStruct.flag = Short.parseShort(strArr[3]);
+
+                        Previousflag = Short.parseShort(strArr[3]);
+
+                        return latLongFixStruct;
+
+                    } else if (callOrigin == 1) {
+                        if (Previousflag != Short.parseShort(strArr[3])) {
+                            latLongFixStruct.latitude = Long.parseLong(strArr[0]);
+                            latLongFixStruct.longitude = Long.parseLong(strArr[1]);
+                            latLongFixStruct.fix = Short.parseShort(strArr[2]);
+                            latLongFixStruct.flag = Short.parseShort(strArr[3]);
+
+                            Previousflag = Short.parseShort(strArr[3]);
+
+                            return latLongFixStruct;
+                        }
+                        return null;
+                    }
+                    return null;
+                }
+                return null;
+            }
+            return null;
+        }
+        return null;
+    }
 
 }
