cc72259ed16d66cecad1d5f9ba2160a85ff662d9
Cocochief <cocochief4@gmail.com>
2022-08-11 15:28:46 -0700

    cleaned up

--- RoboRIO/src/main/java/frc/robot/Robot.java
+++ RoboRIO/src/main/java/frc/robot/Robot.java
@@ -7,31 +7,18 @@
 
 package frc.robot;
 
-import frc.robot.*;
-
-import java.util.*;
-import java.time.*;
-
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-
-import java.nio.ByteBuffer;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.I2C.*;
 
 public class Robot extends TimedRobot {
   private DifferentialDrive m_myRobot;
-
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
 
   private static final int leftUpDeviceID = 3; 
   private static final int leftDownDeviceID = 1;
@@ -46,16 +33,10 @@ public class Robot extends TimedRobot {
   private MotorControllerGroup rightGroup;
 
   private EuclideanCoord robotSpeed = new EuclideanCoord(0.0, 0.0);
-  private EuclideanCoord oldDrive = new EuclideanCoord(0.0, 0.0);
-  private double RAMP_MAX = 0.01;
+  private EuclideanCoord currentSpeed = new EuclideanCoord(0, 0);
+  private final double RAMP_MAX = 0.02;
 
   private final I2C RCArduino = new I2C(Port.kOnboard, 4);
-
-  private Clock clock = Clock.systemDefaultZone();
-
-  private DigitalInput testInput = new DigitalInput(0);
-
-  private LatLongFixStruct gpsCoords = new LatLongFixStruct(0L, 0L, (short)0, (short)0);
 
   @Override
   public void robotInit() {
@@ -95,7 +76,6 @@ public class Robot extends TimedRobot {
       m_myRobot = new DifferentialDrive(leftGroup, rightGroup);
 
   }
-  EuclideanCoord currentSpeed = new EuclideanCoord(0, 0);
   int startCooldown = 50; // Timer to force the all of the motors to 0,
                           // as there is a jump for no reason at all
 
@@ -107,40 +87,42 @@ public class Robot extends TimedRobot {
 
   }
 
-  public void teleopPeriodic() {
-    LatLongFixStruct latLongFixStruct = GPSManager.ParseGPSData((byte) 0);
-    if (latLongFixStruct == null) {
-      // System.out.println("null");
-    } else {
-      System.out.println(latLongFixStruct.toString());
-    }
-
-    /*
-    UNCOMMENT WHEN ROBORIO IS PUT BACK ON THE ROBOT
-    //reading from the arduino to the roborio (i2c)
-    byte[] byteArr = new byte[9]; //THE LAST BYTE DOES NOT READ
-    RCArduino.read(4, 9, byteArr);
-    //converting the byte array into the two values of throttle and steering
-    String bytes = new String(byteArr);
-    String steeringS = bytes.substring(0, 4);
-    String throttleS = bytes.substring(4, 8);
-    //System.out.println(steeringS + ", " + throttleS);
-    double throttle =  Double.parseDouble(throttleS);
-    double steering =  Double.parseDouble(steeringS);
-
-      if (startCooldown < 0) {
-      TeleopMath control =  new TeleopMath(steering, throttle);
+  private void TeleopDrive(double throttle, double steering) {
+    TeleopMath control =  new TeleopMath(steering, throttle);
 
       robotSpeed = new EuclideanCoord(control.RcToDifferential().xEuclid, control.RcToDifferential().yEuclid);
       System.out.println(robotSpeed.toString());
 
+      // Ramp rate
       currentSpeed = control.CalcRamp(currentSpeed, robotSpeed, RAMP_MAX);
-    
-      //System.out.println(currentSpeed.toString());
+
       //Run the Motors
       m_myRobot.tankDrive(-1 * currentSpeed.yEuclid, currentSpeed.xEuclid);
+  }
+
+  public void teleopPeriodic() {
+    LatLongFixStruct latLongFixStruct = GPSManager.ParseGPSData((byte) 0);
+    if (latLongFixStruct != null) {
+      System.out.println(latLongFixStruct.toString());
     }
-    */
+
+    //reading from the arduino to the roborio (i2c)
+    byte[] byteArr = new byte[9]; //THE LAST BYTE DOES NOT READ
+    RCArduino.read(4, 9, byteArr);
+
+    //converting the byte array into the two values of throttle and steering
+    String bytes = new String(byteArr);
+
+    // Parse I2C data from the PWM Arduino
+    String steeringS = bytes.substring(0, 4);
+    String throttleS = bytes.substring(4, 8);
+    double throttle =  Double.parseDouble(throttleS);
+    double steering =  Double.parseDouble(steeringS);
+
+      // Set a cooldown before starting the motors
+      if (startCooldown < 0) {
+      TeleopDrive(throttle, steering);
+    }
 
     startCooldown -= 1;
   }
