fdda720b24b91f77f7f500b8b8a3795469da6a09
Cocochief <cocochief4@gmail.com>
2022-08-14 16:38:10 -0700

    no errors, need to test motorEncoder

--- RoboRIO/src/main/java/frc/robot/motorEncoder.java
+++ RoboRIO/src/main/java/frc/robot/motorEncoder.java
@@ -3,12 +3,21 @@ package frc.robot;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 
-public class Encoder {
+public class motorEncoder {
+    protected final int SPIN_PER_REVOLUTION = 125;
+
     CANSparkMax motor;
     RelativeEncoder encoder;
+    RelativeEncoder altEncoder;
 
-    protected Encoder(CANSparkMax Motor) {
+    protected motorEncoder(CANSparkMax Motor) {
         motor = Motor;
         encoder = motor.getEncoder();
+        altEncoder = motor.getAlternateEncoder(SPIN_PER_REVOLUTION);
+        altEncoder.setVelocityConversionFactor(SPIN_PER_REVOLUTION);
+    }
+
+    protected void getDistance() {
+        System.out.println(altEncoder.getVelocity());
     }
 }
