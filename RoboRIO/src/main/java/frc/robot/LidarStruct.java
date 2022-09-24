9128c258962c7dcb3d033af4714ac37d38951253
Cocochief <cocochief4@gmail.com>
2022-09-24 10:23:57 -0700

    comment out lidar (for later use)

--- RoboRIO/src/main/java/frc/robot/LidarStruct.java
+++ RoboRIO/src/main/java/frc/robot/LidarStruct.java
@@ -1,21 +1,21 @@
-package frc.robot;
+// package frc.robot;
 
-public class LidarStruct {
-    float distance;
-    float angle;
-    byte quality;
+// public class LidarStruct {
+//     float distance;
+//     float angle;
+//     byte quality;
 
-    public LidarStruct(float Distance, float Angle, byte Quality) {
-        distance = Distance;
-        angle = Angle;
-        quality = Quality;
-    }
+//     public LidarStruct(float Distance, float Angle, byte Quality) {
+//         distance = Distance;
+//         angle = Angle;
+//         quality = Quality;
+//     }
 
-    public String toString() {
-        String string = new String();
-        string = Float.toString(distance) + "," + Float.toString(angle) + "," + Byte.toString(quality);
+//     public String toString() {
+//         String string = new String();
+//         string = Float.toString(distance) + "," + Float.toString(angle) + "," + Byte.toString(quality);
 
-        return string;
-    }
+//         return string;
+//     }
 
-}
+// }
