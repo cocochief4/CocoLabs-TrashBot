d0353c06aa6a583db5d81727090570a3b084a48f
cqtqlyst <aayush.gaywala@gmail.com>
2022-11-29 22:53:46 -0800

    Beginning to work on calling the python file?

---
+++ RoboRIO/src/main/java/ml/ml.java
@@ -0,0 +1,36 @@
+package ml;
+
+import java.io.*;
+import java.util.*;
+
+public class ml {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+// import java.io.*;
+ 
+//class test1{
+//  public static void main(String a[]){
+//      try{
+//
+//          String prg = "import sys\nprint int(sys.argv[1])+int(sys.argv[2])\n";
+//          BufferedWriter out = new BufferedWriter(new FileWriter("test1.py"));
+//          out.write(prg);
+//          out.close();
+//          int number1 = 10;
+//          int number2 = 32;
+//          Process p = Runtime.getRuntime().exec("python test1.py "+number1+" "+number2);
+//          BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+//          int ret = new Integer(in.readLine()).intValue();
+//          System.out.println("value is : "+ret);
+//      }
+//      catch(Exception e){}
+//  
+//
+//  }
+//
+//
+//}
