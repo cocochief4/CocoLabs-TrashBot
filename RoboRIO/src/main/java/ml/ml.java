2ce310a9a87a17bf868623e7d83392d0e1266681
cqtqlyst <aayush.gaywala@gmail.com>
2022-12-17 13:08:17 -0800

    Some simple beginning code

--- RoboRIO/src/main/java/ml/ml.java
+++ RoboRIO/src/main/java/ml/ml.java
@@ -5,32 +5,31 @@ import java.util.*;
 
 public class ml {
     public static void main(String[] args) {
+
+        //creating File working directory bc python file requires relative path
         
+        File wd = new File("C:\\Users\\aayus\\Documents\\GitHub\\CocoLabs-TrashBot\\ML Model\\CocoLabs TensorFlow\\example");
+
+        //creating the command to run on the command line
+
+        String[] cmd = {
+            "python", //python
+            "tf_example.py", //relative to working directory
+            "\"C:\\Users\\aayus\\Documents\\GitHub\\CocoLabs-TrashBot\\ML Model\\TestForMLModel.jpg\"" //the argument (the image) (needed as an absolute path not relative)
+
+        };
+
+        //keeping this here because not relative to wd "C:\\Users\\aayus\\Documents\\GitHub\\CocoLabs-TrashBot\\ML Model\\CocoLabs TensorFlow\\example\\tf_example.py"
+
+        //actually trying the command
+        //try/catch incase of an io exception (cus if I screwed up above)
+
+        try {
+            Runtime.getRuntime().exec(cmd, null, wd);
+        } catch (IOException e) {
+            System.out.println("" + e);
+            e.printStackTrace();
+        }
+    
     }
 }
-
-
-// import java.io.*;
- 
-//class test1{
-//  public static void main(String a[]){
-//      try{
-//
-//          String prg = "import sys\nprint int(sys.argv[1])+int(sys.argv[2])\n";
-//          BufferedWriter out = new BufferedWriter(new FileWriter("test1.py"));
-//          out.write(prg);
-//          out.close();
-//          int number1 = 10;
-//          int number2 = 32;
-//          Process p = Runtime.getRuntime().exec("python test1.py "+number1+" "+number2);
-//          BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-//          int ret = new Integer(in.readLine()).intValue();
-//          System.out.println("value is : "+ret);
-//      }
-//      catch(Exception e){}
-//  
-//
-//  }
-//
-//
-//}
